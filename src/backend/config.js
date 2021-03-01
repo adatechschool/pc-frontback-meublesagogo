df8c5dab8e6097a6da8014a498ec86e77275f1b3
root <root@LAPTOP-SAER174R.localdomain>
2021-03-01 15:47:24 +0100

    bug fix windows

--- src/backend/config.js
+++ src/backend/config.js
@@ -3,5 +3,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  MONGODB_URL: `mongodb+srv://${process.env.username}:${process.env.password}@adatech.sbjlq.mongodb.net/site_meubles?retryWrites=true&w=majority`
+  MONGODB_URL: 'mongodb+srv://'+process.env.DB_USERNAME+':'+process.env.DB_PASSWORD+'@adatech.sbjlq.mongodb.net/site_meubles?retryWrites=true&w=majority'
 };
