4f88c524974c33290cd851fa965e08da717e11d4
Margaux Lenne <margaux.lennebonety@gmail.com>
2021-03-03 17:02:53 +0100

    debut securite password

--- src/backend/routes/userRoute.js
+++ src/backend/routes/userRoute.js
@@ -1,12 +1,18 @@
 import User from '../models/userModel.js';
 import express from 'express';
 import bodyParser from 'body-parser';
+import bcrypt from 'bcrypt';
 
 
 const router = express.Router();
 
 router.post('/signup', async (req, res) => {
-    const createUser = await new User({...req.body})
+    const hash = await bcrypt.hash(req.body.password, 10);
+    const createUser = await new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hash
+    })
     if (createUser) {
         res.sendStatus(200);
         createUser.save();
@@ -19,8 +25,11 @@ router.post('/signup', async (req, res) => {
 router.post('/login', async(req, res) => {
     const findUser = await User.findOne({ email: req.body.email })
     if(findUser){
-        if (req.body.password == findUser.password){
-            res.status(200).send({message :"autorisation de se connecter :)"});
+        if (await bcrypt.compare(req.body.password, findUser.password)== true){
+            res.status(200).json({
+                userId: findUser._id,
+                token:"TOKEN"
+            });
         } else {
             res.status(401).send({message :"Mauvais mot de passe :(!"});
         }
