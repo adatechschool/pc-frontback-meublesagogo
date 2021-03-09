30aef0e9c3669cf7bd6bd22fef51b39dc38c99da
aliceKatkout <alice.katkout@gmail.com>
2021-03-09 14:57:07 +0100

    signup begin ok

--- src/frontend/scriptInscription.js
+++ src/frontend/scriptInscription.js
@@ -1,23 +1,21 @@
-let ipAdress = "192.168.1.14."
+let ipAdress = "192.168.1.31."
 
 $(document).ready(() => {
-  //let searchParams = new URLSearchParams(window.location.search);
-  //let id = "";
-//  if (searchParams.has('id')) {
-    //id = searchParams.get('id');
-//  }
 
-$("#submit").click(function(){
-  $.ajax({
+  $("#submit").click(function(){
+    if ($("#gridCheck").val() == true){
+    $.ajax({
     // The URL for the request
-    url: "http://"+ipAdress+":5000/api/user/signup",
+      url: "http://"+ipAdress+":5000/api/user/signup",
 
     // Whether this is a POST or GET request
-    type: "POST",
+      type: "POST",
 
     // The type of data we expect back
-    dataType: "json",
-    data: "name="+$("#name")+"&email="+$("#email")+"&password="+$("#password")
+      dataType: "json",
+      data: "name="+$("#name").val()+"&email="+$("#email").val()+"&password="+$("#password").val()
+
+    })
+  }
   })
-  })
-  })
+})
